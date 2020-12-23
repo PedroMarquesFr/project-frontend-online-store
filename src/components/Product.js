ccff8195233c4dcb97743d8ffb7274ef53e8b016
Pedro Marques <pedromarques1011@gmail.ocm>
2020-12-22 21:27:09 -0300

    done cartItems redux

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+import {addProduct} from '../store/ducks/CartItems/actions';
+
 import '../styles/product.css';
 import { MdLocalShipping } from 'react-icons/md';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import addToCart from '../services/addToCart';
+// import addToCart from '../services/addToCart';
 
 class Product extends Component {
   constructor(props) {
@@ -12,49 +16,36 @@ class Product extends Component {
   }
 
   addToCart() {
-    const {
-      title,
-      thumbnail,
-      price,
-      id,
-      category,
-      searchKey,
-      updateCartTotal,
-      availableQuantity,
-    } = this.props;
-    addToCart({ title, thumbnail, price, id, category, searchKey, availableQuantity });
-    updateCartTotal();
+    const { title, thumbnail, price, id, availableQuantity, addProduct} = this.props;
+    addProduct({ title, thumbnail, price, id, availableQuantity, quantity:1});
   }
 
   render() {
     const { title, thumbnail, price, id, freeShipping } = this.props;
     return (
       <div data-testid="product">
-        <Link
-          data-testid="product-detail-link"
-          to={ `./details/${id}` }
-        >
+        <Link data-testid="product-detail-link" to={`./details/${id}`}>
           {freeShipping ? (
             <p className="p-free-shipping">
               <MdLocalShipping data-testid="free-shipping" />
               Envio grátis
             </p>
           ) : null}
-          <img src={ thumbnail } alt="thumb-product" />
+          <img src={thumbnail} alt="thumb-product" />
           <h3 className="h3-title">{title}</h3>
           <p className="p-price">{price}</p>
         </Link>
-        <button
-          type="button"
-          data-testid="product-add-to-cart"
-          onClick={ this.addToCart }
-        >
+        <button type="button" data-testid="product-add-to-cart" onClick={this.addToCart}>
           Adicionar ao carrinho
         </button>
       </div>
     );
   }
 }
+
+const mapDispatchToProps = { addProduct };
+
+export default connect(null, mapDispatchToProps)(Product);
 
 Product.propTypes = {
   title: PropTypes.string.isRequired,
@@ -72,4 +63,3 @@ Product.defaultProps = {
   category: undefined,
 };
 
-export default Product;
--- src/store/ducks/CartItems/index.js
+++ src/store/ducks/CartItems/index.js
@@ -7,22 +7,49 @@ const initialState = {
       id: 'MLB1365396415',
       title: 'Prancha De Cabelo Lizze Extreme Cinza 110v',
       price: 465,
-      available_quantity: 5,
+      availableQuantity: 5,
+      quantity: 1,
+      thumbnail: 'https://thumb.com',
     },
     {
       id: 'MLB1365396416',
       title: 'Prancha De Cabelo Lizze Extreme Cinza 110v',
       price: 465,
-      available_quantity: 5,
+      availableQuantity: 5,
+      quantity: 1,
+      thumbnail: 'https://thumb.com',
     },
   ],
 };
 
+
+function isBiggerThem(state,index){
+  const added = state.cart[index].quantity;
+  const maxQuantity = state.cart[index].availableQuantity
+  if(added>maxQuantity){
+    state.cart[index].quantity = maxQuantity; 
+    return true;
+  }
+  return false;
+}
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case TasksTypes.ADD:
+      const index = state.cart.findIndex(({ id }) => id === action.prodObject.id);
+      if (index > -1) {
+        state.cart[index].quantity += 1;
+        isBiggerThem(state,index);
+        return { ...state };
+      }
       return { ...state, cart: [...state.cart, action.prodObject] };
     case TasksTypes.EDIT:
+      state.cart.find(({ id, quantity }, index) => {
+        if (id === action.prodId) {
+          state.cart[index].quantity = action.boolWillAdd ? (quantity += 1) : (quantity -= 1); //mudo jaja
+          return true;
+        }
+        return false;
+      });
       return { ...state, cart: [...state.cart] };
     case TasksTypes.DELETE:
       let newArray = state.cart;
